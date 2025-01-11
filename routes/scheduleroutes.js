5a1529604f7342ac23635afadb8b12fc9b2f5292
Stephen McLean <150471173+SM-SMcLean@users.noreply.github.com>
2025-01-11 14:12:49 +0000

    Update scheduleroutes.js formatting
    - Removed spacing to improve readability

--- routes/scheduleroutes.js
+++ routes/scheduleroutes.js
@@ -2,17 +2,11 @@ const express = require('express');
 const controller = require('./../controllers/schedulecontrollers');
 const router = express.Router();
 
-
 router.get('/', controller.getSchedule);
-
 router.get('/new', controller.getAddNewRun);
-
 router.get('/edit/:id', controller.selectRun);
-
 router.post('/new', controller.postNewRun);
-
 router.post('/edit/:id', controller.updateRun);
-
 router.post('/del/:id', controller.deleteRun);
 
 module.exports = router;
